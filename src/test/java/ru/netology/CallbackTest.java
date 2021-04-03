bb51b5493001321f27006a71cd8fd958d8a8f606
mona52 <mona52@list.ru>
2021-04-03 15:01:54 +0300

    create test

--- src/test/java/ru/netology/CallbackTest.java
+++ src/test/java/ru/netology/CallbackTest.java
@@ -4,18 +4,19 @@ import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.ElementsCollection;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.exactText;
+
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
-public class CallbackTest {
+class CallbackTest {
 
     @Test
     void shouldSubmitRequest() {
-        open ("http://localchost:9999");
-        ElementsCollection inputs = $$ (".input.control");
+        open ("http://localhost:9999");
+        ElementsCollection inputs = $$ (".input__control");
         inputs.find(Condition.attribute("type","text")).setValue("Иванов Николай");
         inputs.find(Condition.attribute("type","tel")).setValue("+79850000000");
-        $(".checkbox_box").click();
+        $(".checkbox__box").click();
         $(".button").click();
         $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
